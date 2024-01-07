37e8f5e0f4d4bff59d6f0f6086448aa7c32dacb7
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-07 12:31:37 +0545

    uuid addition

--- camera.py
+++ camera.py
@@ -1,6 +1,8 @@
 import cv2
+import uuid
 cap = cv2.VideoCapture(0)
 result, frame = cap.read()
+name = str(uuid.uuid4())+".jpeg"
 print(result)
-cv2.imwrite("abc.jpeg",frame)
+cv2.imwrite(name,frame)
 cap.release()
