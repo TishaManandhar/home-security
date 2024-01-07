beadb81467b075f04bf34deb1782952bd6e0fe8b
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-07 13:02:31 +0545

    square around person detection

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 import cv2
 import uuid
+import numpy as np
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cap = cv2.VideoCapture(0)
@@ -14,6 +15,15 @@ cv2.imwrite(name,frame)
 frame = cv2.resize(frame, (640,480))
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 boxes,weights = hog.detectMultiScale(gray, winStride=(8,8))
-print(weights)
-print(boxes)
+boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(gray, (xA, yA), (xB, yB), (0, 255, 0), 2)
+if boxes:
+    print(weights)
+    print(boxes)
+    print("Person Detected.")
+    cv2.imwrite(str(uuid.uuid4())+"-detected.jpeg", gray)
+else:
+    print("Person Not Detected.")
 cap.release()
