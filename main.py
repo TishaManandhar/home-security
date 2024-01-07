075ac6f7e48f4b9df69f39bc65753255f9c4c7da
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-07 16:25:52 +0545

    threading

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import threading
 import time
+import motion
 
 def looper():
     while True:
         time.sleep(2)
-        print("Hello")
+        is_motion = motion.motion_detect()
+        if is_motion:
+            print("motion detected")
+        else:
+            print("motion not detected")
 
 if __name__ == "__main__":
     t = threading.Thread(target=looper)
+    t.start()
     print("running main thread")
