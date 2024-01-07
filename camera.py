09cb86675228f8c343632b82a45019d8e485902c
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-07 12:36:27 +0545

    added comments

--- camera.py
+++ camera.py
@@ -1,8 +1,11 @@
 import cv2
 import uuid
 cap = cv2.VideoCapture(0)
+#capturing frame and providing result
 result, frame = cap.read()
+#radom uuid generation
 name = str(uuid.uuid4())+".jpeg"
 print(result)
+#writting image to a file
 cv2.imwrite(name,frame)
 cap.release()
