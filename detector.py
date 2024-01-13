62b604db29ca25b0b7d59647971a60dc4a06bc97
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-13 13:35:39 +0545

    a

--- detector.py
+++ detector.py
@@ -1,4 +1,6 @@
 import cv2
+import imutils
+
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cap = cv2.VideoCapture(0)
@@ -8,7 +10,8 @@ def detect_person():
     # capturing frame and providing result
     result, frame = cap.read()
     if result:
-        frame = cv2.resize(frame, (640, 480))
+        #frame = cv2.resize(frame, (640, 480))
+        frame = imutils.resize(frame, width=min(400, frame.shape[1]))
         #cv2.imshow('frame', frame)
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         boxes, weights = hog.detectMultiScale(frame, winStride=(4, 4), padding=(4, 4), scale=1.05)
