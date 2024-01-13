b86179c710269d7c72b5258c2d075a2e59697eda
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-13 12:29:56 +0545

    person not detected

--- detector.py
+++ detector.py
@@ -1,23 +1,21 @@
 import cv2
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+cap = cv2.VideoCapture(0)
 
 
 def detect_person():
-    cap = cv2.VideoCapture(0)
     # capturing frame and providing result
     result, frame = cap.read()
     if result:
         frame = cv2.resize(frame, (640, 480))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
-        cap.release()
         if len(boxes) > 0:
             return True
         else:
             return False
     else:
-        cap.release()
         print("Could not capture frame")
         return False
 
