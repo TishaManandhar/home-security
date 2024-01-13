e357a3466490ea99be8e9ed4017aa37649dba383
TishaManandhar <tishamanandhar12@gmail.com>
2024-01-13 13:19:19 +0545

    frame capture

--- detector.py
+++ detector.py
@@ -8,10 +8,10 @@ def detect_person():
     # capturing frame and providing result
     result, frame = cap.read()
     if result:
-        #frame = cv2.resize(frame, (640, 480))
+        frame = cv2.resize(frame, width=min(400, frame.shape[1]))
         #cv2.imshow('frame', frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        boxes, weights = hog.detectMultiScale(frame, winStride=(4, 4), padding=(4, 4), scale=1.05)
         if len(boxes) > 0:
             return True
         else:
